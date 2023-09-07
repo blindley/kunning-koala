c0d24b3702be81f90bd4e18a4afa61f7b7e55e3b
Benjamin Lindley <benjameslindley@gmail.com>
2023-09-07 18:50:54 -0500

    sign in with test user

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import { useContext, useState } from "react";
 import { createContext } from "react";
 import SignIn from "./SignIn";
+import "./firebaseInit"; // only import to run initialization, no items to use
 
 export const UserContext = createContext(null);
 
 const Header = () => {
   const { user } = useContext(UserContext);
 
-  return <>{user ? <h1>Hello, {user}</h1> : <h1>Hello, Guest</h1>}</>;
+  return (
+    <>
+      {user ? <h1>Hello, {user}</h1> : <h1>Hello, Guest</h1>}
+      <hr />
+    </>
+  );
 };
 
 function App() {
--- src/SignIn.jsx
+++ src/SignIn.jsx
@@ -1,18 +1,36 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { UserContext } from "./App";
+import { AuthErrorCodes, getAuth, signInWithEmailAndPassword } from "firebase/auth";
 
 const SignIn = () => {
   const { user, setUser } = useContext(UserContext);
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e, faulty = false) => {
     e.preventDefault();
-    setUser("Benjamin");
+
+    const auth = getAuth();
+    const email = "test@test.test";
+    const password = faulty ? "x" : "testpassword";
+
+    try {
+      const cred = await signInWithEmailAndPassword(auth, email, password);
+      console.log(cred.user);
+    } catch (error) {
+      if (error.code == AuthErrorCodes.INVALID_PASSWORD) {
+        console.log("Error: invalid password");
+      } else {
+        console.log(error);
+      }
+    }
   };
 
   return (
     <div>
       <form onSubmit={handleSubmit}>
         <button type="submit">Sign In</button>
+      </form>
+      <form onSubmit={(e) => handleSubmit(e, true)}>
+        <button type="submit">Faulty Sign In</button>
       </form>
     </div>
   );
